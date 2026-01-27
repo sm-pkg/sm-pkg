b349e10f618a0bca40df2c8b1c1f94a7f02fb73e
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-01-26 18:42:52 -0700

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
+
+  pkgs = import nixpkgs {
+    config = {};
+    overlays = [];
+  };
+in
+  pkgs.mkShell {
+    hardeningDisable = ["fortify"];
+    buildInputs = with pkgs; [
+      pkg-config
+      openssl
+      rust-analyzer
+      cargo-audit
+      gcc
+      gnumake
+    ];
+  }
