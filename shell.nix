b55a3d89518bc47ca56c86791bd5b32d34f39560
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-01-28 21:19:14 -0700

    Add initial goreleaser config

--- shell.nix
+++ shell.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
 
@@ -7,14 +8,19 @@ let
   };
 in
 pkgs.mkShell {
+  LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.openssl ];
   hardeningDisable = [ "fortify" ];
   buildInputs = with pkgs; [
     pkg-config
     openssl
+    cmake
+    zlib
+    libgit2
     rust-analyzer
     cargo-audit
     gcc
     gnumake
     goreleaser
+    zig # required by goreleaser
   ];
 }
